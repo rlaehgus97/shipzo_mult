b6b22da370b8b7d9b55b761501e1280d503cc126
rlaehgus97 <starkim625@gmail.com>
2024-07-17 11:09:30 +0900

    mult update

--- src/shipzo_mult/shipzo_mult.py
+++ src/shipzo_mult/shipzo_mult.py
@@ -1,4 +1,13 @@
-#곱하기
+import sys
 
-def mult(a,b):
-    print(a*b)
+def mult():
+
+    print(int(b)*int(c))
+
+a = sys.argv[0]
+b = sys.argv[1]
+c = sys.argv[2]
+
+#print(f"a={a}, b={b}, c={c}")
+
+#mult()
